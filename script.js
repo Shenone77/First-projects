4ef26a67893c7b05963e9e93a68a02bfc975edb4
sharavan <164636285+Shenone77@users.noreply.github.com>
2024-11-01 12:58:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const sections = document.querySelectorAll("section");
+    
+    sections.forEach(section => {
+        section.addEventListener("click", () => {
+            section.classList.toggle("active");
+            section.classList.toggle("expanded");
+        });
+    });
+});
